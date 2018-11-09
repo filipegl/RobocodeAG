76b1eb8ce177bd09d0a5307d35767af537df7b7f
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2018-11-09 17:22:35 -0300

    Criacao do projeto. Criacao de classes

---
+++ src/ag/AlgoritmoGenetico.java
@@ -0,0 +1,16 @@
+package ag;
+
+
+public class AlgoritmoGenetico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public Cromossomo[] geraPopulacao() {
+		//codigo de @thiaguin
+		return null;
+		
+	}
+ 
+}
---
+++ src/ag/Cromossomo.java
@@ -0,0 +1,19 @@
+package ag;
+
+public class Cromossomo {
+	
+	public void setGene(int[] gene) {
+		//Aqui seta o array dos valores.
+		
+	}
+	
+	public int[] getGene() {
+		//Aqui retorna o array dos valores.
+		return null;
+	}
+	
+	public int getFitness() {
+		return 0;	
+	}
+	
+}
