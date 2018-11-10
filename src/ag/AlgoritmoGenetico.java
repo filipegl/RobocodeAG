f53e1890b6244bc230f2ca18d5f5452c13e86112
thiaguin <jose.thiago.silva@ccc.ufcg.edu.br>
2018-11-10 12:50:32 -0300

    gerando populacao

--- src/ag/AlgoritmoGenetico.java
+++ src/ag/AlgoritmoGenetico.java
@@ -2,15 +2,49 @@ package ag;
 
 
 public class AlgoritmoGenetico {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String mostra = printString(geraPopulacao());
+		System.out.println(mostra);
 	}
-	public Cromossomo[] geraPopulacao() {
-		//codigo de @thiaguin
-		return null;
-		
+	
+	
+	public static Cromossomo[] geraPopulacao() {
+		Cromossomo[] populacao = new Cromossomo[324];
+		int index = 0;
+		for (int a = 1; a <= 3; a++) {
+			int firstGrau = 0;
+			for (int b = 0; b < 3; b++) {
+				int secondGrau = 0;
+				for (int c = 0; c < 3; c++) {
+					int thirdGrau = 0;
+					for (int d = 0; d < 3; d++) {
+						int ahead = 10;
+						for (int e = 0; e < 2; e++) {
+							int walk = 10;
+							for (int f = 0; f < 2; f++) {
+								populacao[index] = new Cromossomo(index, a, firstGrau, secondGrau, thirdGrau, ahead, walk);
+								index++;
+								walk = 50000;
+							}
+							ahead = 50000;
+						}
+						thirdGrau += 180;
+					}
+					secondGrau += 180;
+				}
+				firstGrau += 180;
+			}
+		}
+		return populacao;
 	}
  
+	public static String printString(Cromossomo[] a) {
+		String result = "";
+		for (Cromossomo As: a) {
+			result += As.toString() ;
+		}
+		return result;
+		
+	}
 }
--- src/ag/Cromossomo.java
+++ src/ag/Cromossomo.java
@@ -2,6 +2,22 @@ package ag;
 
 public class Cromossomo {
 	
+	private int index;
+	private int[] genes;
+	
+	
+	public Cromossomo(int index, int fire, int firstGrau, int secondGrau, int thirdGrau, int ahead, int walk) {
+		this.index = index;
+		this.genes = new int[6];
+		this.genes[0] = fire;
+		this.genes[1] = firstGrau;
+		this.genes[2] = secondGrau;
+		this.genes[3] = thirdGrau;
+		this.genes[4] = ahead;
+		this.genes[5] = walk;
+			
+	}
+
 	public void setGene(int[] gene) {
 		//Aqui seta o array dos valores.
 		
@@ -16,4 +32,10 @@ public class Cromossomo {
 		return 0;	
 	}
 	
+	public String toString() {
+		String result = "";
+		result += "[" + genes[0] + ", " + genes[1] + ", " + genes[2] + ", " + genes[3] + ", " + genes[4] + ", " + genes[5] + "]\n" ;
+		return result;
+		
+	}
 }
